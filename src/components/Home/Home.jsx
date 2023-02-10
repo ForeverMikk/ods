a8ebb74c38fba8770b9848b2242e81a42858ade6
unknown <michaelmolina679@gmail.com>
2023-02-10 12:43:09 -0600

    add Home and Timer view and Tailwind Css

---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import Timer from './Timer/Timer';
+
+
+const Home = () => {
+    return (
+    <>
+        <h1 className="text-3xl font-bold underline">
+            Hello world!
+        </h1>
+        <h1>
+            Hello world!
+        </h1>
+        <Timer />
+    </>
+    )
+}
+
+export default Home;
